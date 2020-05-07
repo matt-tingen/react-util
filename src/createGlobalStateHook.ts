d8ac47459126438ab7b4adbc719e30b3b704d08a
Matt Tingen <matt.tingen@gmail.com>
2020-05-07 01:52:31 -0400

    Fix babel consumption
    Importing from the root was causing a babel error in a consuming project:
    
    ```
    Property object of MemberExpression expected node to be of a type ["Expression"] but instead got null
    ```
    
    This error appears to because by dynamic `require` usage, but the
    offending code only had static `require` usage so I'm not quite sure
    what was wrong.

--- src/createGlobalStateHook.ts
+++ src/createGlobalStateHook.ts
@@ -1,8 +1,8 @@
-import { createGlobalState } from 'react-use';
+import createGlobalState from 'react-use/lib/createGlobalState';
 import { StatePair } from './utils';
 
-// This exists separately from `react-use`'s `createGlobalState` because that
-// function ORs `undefined` into the state type unconditionally.
+// This is re-exported because `react-use`'s `createGlobalState` ORs `undefined`
+// into the state type unconditionally.
 const createGlobalStateHook = createGlobalState as <S>(
   initialState: S,
 ) => () => StatePair<S>;
