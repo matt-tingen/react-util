627b091a321b9bc8cae4a5c7f1d7a67e26f9d092
Matt Tingen <matt.tingen@gmail.com>
2022-01-29 15:44:39 -0500

    Remove state wrapper hooks
    These have replacements in `react-use`, `@react-hookz/web` aside from
    the pluggable variants which can be implemented in user land without
    much effort.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,4 @@
 export { createStatefulContext } from './createStatefulContext';
-export { useToggle, usePluggableToggle } from './useToggle';
-export { useCounter, usePluggableCounter } from './useCounter';
-export { useMethods, usePluggableMethods } from './useMethods';
 export { useAsyncFn } from './useAsyncFn';
 export { useAsync } from './useAsync';
 export { transformAsyncState } from './transformAsyncState';
