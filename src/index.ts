4eb35af3376c6e204a2c07075547a5b696442e6b
Matt Tingen <matt.tingen@gmail.com>
2023-03-05 13:13:36 -0500

    Remove useAutoAsync
    The wrapping of other hook libraries has been a maintenence thorn due to
    frequent breaking changes. The breaking changes didn't often impact this
    library, but the peer-dep constraint made it difficult to update this
    library nonetheless.
    
    This hook was the last hold-out and `react-query` or similar is
    typically a better solution. If nothing else, the "auto" portion is
    relatively easily achievable with an effect.

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,5 @@ export { createContext } from './createContext';
 export { createStrictContext } from './createStrictContext';
 export { createStatefulContext } from './createStatefulContext';
 export { createStrictStatefulContext } from './createStrictStatefulContext';
-export { useAutoAsync } from './useAutoAsync';
-export type { AutoAsyncState } from './useAutoAsync';
 export { useSlice } from './useSlice';
 export { useHandler } from './useHandler';
